7b4a97a073df34f56d7ce4b6b5c5a9742625d9a5
Mario Esteban Hernandez <hdez.marioe@gmail.com>
2018-11-26 17:53:08 -0600

    Inicio de sesion

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,42 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('M_consultas');
+
+    }
+
+    public function index()
+    {
+        $this->load->view('welcome_message');
+    }
+    public function iniciosesion()
+    {
+        $usuario = $this->input->post('usuario');
+        $pass = $this->input->post('pass');
+        $query = $this->M_consultas->login($usuario, $pass);
+
+        foreach ($query->result() as $key) {
+
+            $usuario = array(
+                'ID' => $key->user_ID,
+                'usuario' => $key->user_usuario,
+                'nombre' => $key->user_nombre,
+            );
+
+            $this->session->set_userdata($usuario);
+
+            redirect('Panel', 'refresh');
+
+        }
+
+        redirect('Welcome', 'refresh');
+
+    }
+
 }
